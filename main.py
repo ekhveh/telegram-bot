06cb01f2b4efacc0a7d2eabb1e99f145d648795e
ekhveh <145249979+ekhveh@users.noreply.github.com>
2025-07-09 23:26:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def get_joke_from_chatgpt():
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "تو یه کمدین باحال هستی"},
-                {"role": "user", "content": "یه جوک بامزه و خفن درباره کل کل فرزندان با پدر و مادر یا کل کل خواهر برادر یا کل کل دختر پسرها بگو"}
+                {"role": "user", "content": "یه جوک بامزه و خفن درباره سربسر گذاشتن فرزندان با پدر و مادر یا  خواهر برادر یا  دختر و پسرها بگو"}
             ],
             max_tokens=4000,
             temperature=0.8
